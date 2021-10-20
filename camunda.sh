64eb91b869753eb11b7dc313691ffe95c4573d05
Egor Krasnov <nikegor24@gmail.com>
2021-10-20 18:30:23 +0300

    HCX-201 Rename env variable in camunda.sh

--- camunda.sh
+++ camunda.sh
@@ -104,7 +104,7 @@ export HISTORY_LEVEL="${HISTORY_LEVEL:-${BPM_HISTORY_LEVEL:-none}}"
 
 XML_PLATFORM_NAMESPACE="http://www.camunda.org/schema/1.0/BpmPlatform"
 XML_PLATFORM_PROPERTIES="//xmlns:bpm-platform/xmlns:process-engine/xmlns:properties/xmlns:property"
-XML_HISTORY_LEVEL="${XML_PLATFORM_PROPERTIES}[@name='history']"
+XML_PLATFORM_HISTORY_LEVEL="${XML_PLATFORM_PROPERTIES}[@name='history']"
 
 echo "Configure bpm platform"
 xmlstarlet ed -L \
